e1397bf15b2cb27ae7418d533e1113cc4fa6f73c
RAI015 <i.scryed72342019@gmail.com>
2020-04-27 19:16:53 +0900

    S3用設定の追加

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      provider: 'AWS',
+      region: 'ap-northeast-1',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+    }
+    config.fog_directory = Rails.application.credentials.aws[:s3_bucket]
+  end
+end
