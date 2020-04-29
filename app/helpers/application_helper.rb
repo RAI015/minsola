de174a22deea64e170761b79fd8320a4b4bb830e
RAI015 <i.scryed72342019@gmail.com>
2020-04-29 22:02:28 +0900

    テストデータの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   # PostのSelectBoxを定義
   WEATHERS = %w[快晴 晴れ 薄曇り 曇り 小雨 雨 豪雨 雷 みぞれ 雪 大雪 あられ ひょう 霧 霧雨 砂あらし].freeze
   FEELINGS = %w[うだる暑さ 暑い 暖かい ちょうどいい 肌寒い 凍えるほど寒い あてはまらない].freeze
-  EXPECTATIONS = %w[今と変化なさそう 回復しそう 下り坂になりそう].freeze
+  EXPECTATIONS = %w[変化なさそう 晴れそう 曇りそう 悪くなりそう].freeze
 
   def full_title(page_title = '')
     base_title = WEBSITE_NAME
