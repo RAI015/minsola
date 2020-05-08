b37f164317ab3eaf6852a9b2918d176a430f2af9
RAI015 <i.scryed72342019@gmail.com>
2020-05-08 20:29:26 +0900

    fix-view#69

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Devise初期設定時に設定
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
